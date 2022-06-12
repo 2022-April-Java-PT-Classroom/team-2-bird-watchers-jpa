5d005b33f151734e72c215b4d43584d0e7dde6b9
ackley787 <ackley787@gmail.com>
2022-06-12 17:45:11 -0400

    intial setup, CountryController, Country, and start of CountryRepo

---
+++ src/main/java/org/wecancodeit/birdwatcher/controller/CountryController.java
@@ -0,0 +1,35 @@
+package org.wecancodeit.birdwatcher.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.wecancodeit.birdwatcher.model.Country;
+import org.wecancodeit.birdwatcher.repo.CountryRepository;
+
+import javax.annotation.Resource;
+
+@Controller
+@RequestMapping("/country")
+public class CountryController {
+
+    @Resource
+    private CountryRepository countryRepo;
+
+    @GetMapping("")
+    public String displayCountries(Model model){
+        model.addAttribute("countries", countryRepo.findAll());
+        return "countries";
+    }
+    @GetMapping("/{id}")
+    public String findOneCountry(Model model, @PathVariable Long id){
+        model.addAttribute("country", countryRepo.findById(id).get());
+        return "country";
+    }
+    @PostMapping("/addCountry")
+    public String addCountry(@RequestParam String countryName,@RequestParam String region, @RequestParam String habitat){
+        Country countryToAdd = new Country(countryName, region, habitat);
+        countryRepo.save(countryToAdd);
+        return "redirect:/countries";
+
+    }
+}
---
+++ src/main/java/org/wecancodeit/birdwatcher/populator.java
@@ -0,0 +1,26 @@
+package org.wecancodeit.birdwatcher;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.wecancodeit.birdwatcher.model.Country;
+import org.wecancodeit.birdwatcher.repo.CountryRepository;
+
+import javax.annotation.Resource;
+
+@Component
+public class populator implements CommandLineRunner {
+
+    @Resource
+    private CountryRepository countryStorage;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Country countryOne = new Country("USA", "North America", "Mountains");
+//        Country countryTwo= new Country("Canada", "North America", "Mountains");
+//
+        countryStorage.save(countryOne);
+//        countryStorage.save(countryTwo);
+    }
+}
