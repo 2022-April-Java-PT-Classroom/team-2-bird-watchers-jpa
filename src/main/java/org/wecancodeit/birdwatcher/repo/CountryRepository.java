d5381d58242a0b80d14b7ab16c00189b0a7602b8
louis2tch <louis2tch@gmail.com>
2022-06-15 00:16:49 -0400

    added ~about us~ and something more

---
+++ src/main/java/org/wecancodeit/birdwatcher/repo/CountryRepository.java
@@ -0,0 +1,9 @@
+package org.wecancodeit.birdwatcher.repo;
+
+
+import org.springframework.data.repository.CrudRepository;
+import org.wecancodeit.birdwatcher.model.Country;
+
+public interface CountryRepository extends CrudRepository<Country, Long> {
+
+}
