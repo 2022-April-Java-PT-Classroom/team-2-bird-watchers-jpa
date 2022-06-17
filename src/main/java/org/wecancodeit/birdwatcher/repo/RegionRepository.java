84de518e7272625b3f45542ffb29f928df6a491f
louis2tch <louis2tch@gmail.com>
2022-06-17 01:38:09 -0400

    all work code at 6/17/2022

--- src/main/java/org/wecancodeit/birdwatcher/repo/RegionRepository.java
+++ src/main/java/org/wecancodeit/birdwatcher/repo/RegionRepository.java
@@ -5,8 +5,5 @@ import org.wecancodeit.birdwatcher.model.Region;
 
 public interface RegionRepository extends CrudRepository<Region, Long> {
 
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> abf5e6f8d04e3bd36aa6e7c231e5a169b27da64e
+
